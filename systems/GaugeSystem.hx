69e5703ad2dc305a9788c600b21509c8f90f91fd
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-04-17 23:08:12 +0200

    Updated

--- systems/GaugeSystem.hx
+++ systems/GaugeSystem.hx
@@ -19,7 +19,7 @@ class GaugeSystem extends System
     private var gaugeNode:GaugeNode;
     public var life:Float = 40;
     private var maxLife:Float = 40;
-    private var steps = [0, 5, 10, 15, 20, 25, 30, 35, 40];
+    private var steps = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40];
 
     public function new()
     {
